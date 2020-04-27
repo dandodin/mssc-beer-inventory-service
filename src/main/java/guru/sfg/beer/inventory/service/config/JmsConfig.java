af8fb02c492d52a8b2c804eb664d8bfd1d77c1ef
DodinD1 <62258853+dandodin@users.noreply.github.com>
2020-04-27 14:12:22 -0700

    save allocate changes

--- src/main/java/guru/sfg/beer/inventory/service/config/JmsConfig.java
+++ src/main/java/guru/sfg/beer/inventory/service/config/JmsConfig.java
@@ -11,6 +11,7 @@ import org.springframework.jms.support.converter.MessageType;
 public class JmsConfig {
 
     public static final String NEW_INVENTORY_QUEUE = "new-inventory";
+    public static final String ALLOCATE_ORDER_QUEUE = "allocate-order";
 
     @Bean
     public MessageConverter messageConverter(ObjectMapper objectMapper) {
